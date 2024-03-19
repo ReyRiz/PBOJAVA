5268bd20566a3c57d141f39b549b1381ea7171ac
Fariz Idham Ramdhani <143472017+ReyRiz@users.noreply.github.com>
2024-03-19 19:19:27 +0700

    Permulaan

--- Pertemuan 1/TP1.java
+++ Pertemuan 1/TP1.java
@@ -1,28 +1,28 @@
-import java.util.Scanner;
+import java.util.Scanner; // Import Library dari java utility kelas Scannernya agar dapat memasukkan Input 
 
-public class TP1 {
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int jumlahBilanganBulat = 0, jumlahBilanganDesimal = 0, jumlahHuruf = 0;
-        int x = input.nextInt();
-        for (int i = 0; i < x; i++){
-            try {
-                try {
+public class TP1 { // Membuat Class yang bernama TP1 yang harus sama dengan nama filenya
+    public static void main(String[] args){ // main method seperti blok utama dan tempat dijalankan seluruh perintah yang ditulis (Singkatnya titik awal program akan dieksekusi)
+        Scanner input = new Scanner(System.in); // membuat objek dari Scanner dan akan digunakan untuk menginput dari user
+        int jumlahBilanganBulat = 0, jumlahBilanganDesimal = 0, jumlahHuruf = 0; // Membuat 3 variabel bertipe data integer dan memberikan nilai 0 kesetiap variabelnya
+        int x = input.nextInt(); // Membuat variabel X yang dimana valuenya berdasarkan dari input user, nextInt maksudnya inputan untuk Int, kalau double (float) pakai nextDouble()
+        for (int i = 0; i < x; i++){ // Perulangan for sebanyak X kali yang diinput, i nya akan berulang selagi i kurang dari X
+            try { // blok try-catch disini artinya mencoba terlebih dahulu dan jika eror atau menangkap sesuatu eror akan masuk ke blok kode catch (Exception e)
+                try { // Nested try karena try pertama untuk menangkap jika erornya karena huruf, dan try kedua digunakan untuk mengalihkan angka jika angkanya bukan int maka ke double;
                     int angka = input.nextInt();
-                    jumlahBilanganBulat++;
-                    angka = angka+1; // Supaya hilang kuning kuning bingungnya
+                    jumlahBilanganBulat++; // Disini maksudnya jika angka yang diinput merupakan angka Int maka Jumlah Bilangan bulat otomatis tambah 1, dan jika double maka turun ke blok catch
+                    angka = angka+1; // Gunanya agar variabel angka pernah digunakan, dapat dihapus
                 } catch (Exception e){
                     double angka = input.nextDouble();
                     angka = angka+1;
-                    jumlahBilanganDesimal++;
+                    jumlahBilanganDesimal++; // Sama dengan diatas
                 }
             } catch (Exception e) {
-                jumlahHuruf++;
+                jumlahHuruf++; //Disini jika memang apa yang kita input bukan merupakan antara int atau double maka dia masuk ke blok catch ini dan akan menambah variabel Jumlah huruf sebanyak 1
             }
         }
-        input.close();
-        if (jumlahHuruf == 0){
-            System.out.println(jumlahBilanganBulat + " Bilangan Bulat\n" + jumlahBilanganDesimal + " Bilangan Desimal");
+        input.close(); // Menutup input agar tak ada lagi yang bisa diinput
+        if (jumlahHuruf == 0){ // Disini hanya membuat kondisi jika memang tidak ada error huruf yang didapat maka yang diprint sisa jumlah bilangan bulat dan juga bilangan desimal, jika ada maka 3 yaitu jumlah int, double, dan huruf
+            System.out.println(jumlahBilanganBulat + " Bilangan Bulat\n" + jumlahBilanganDesimal + " Bilangan Desimal"); // Ingat \n merupakan enter atau baris baru
         } else {
             System.out.println(jumlahBilanganBulat + " Bilangan Bulat\n" + jumlahBilanganDesimal + " Bilangan Desimal\n" + jumlahHuruf + " Huruf/Karakter");
         }
